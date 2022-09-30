1e97ec3d36952c70041ed559abb033855ed9d64e
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2022-09-30 08:47:35 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // Import stylesheets
 import './style.css';
+
+const minToSec = (min) => min * 60;
+
+const aZaroven = (b1, b2) => b1 && b2;
+
+function obsah(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') return -1;
+  return a * b;
+}
+
+console.log(aZaroven(true, true));
+console.log(aZaroven(false, true));
+console.log(obsah(5, 10));
+console.log(obsah('abc', 10));
